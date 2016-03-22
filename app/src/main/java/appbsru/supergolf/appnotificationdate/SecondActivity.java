9664f75419b6a96549a55577e56a4ea54ca9091d
golf <golfpradit@gmail.com>
2016-03-22 10:08:12 +0700

    Test

---
+++ app/src/main/java/appbsru/supergolf/appnotificationdate/SecondActivity.java
@@ -0,0 +1,15 @@
+package appbsru.supergolf.appnotificationdate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by pradit on 20/3/2559.
+ */
+public class SecondActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
